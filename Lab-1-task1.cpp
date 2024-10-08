fbcb5ab025fc6e0acc15db542e4a1adb822bdaa0
Aria <118472622+Workalina@users.noreply.github.com>
2024-10-08 20:31:57 +0300

    using namespace std

--- Lab-1-task1.cpp
+++ Lab-1-task1.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 // lab 1 task 1
 int main()
 {
     double x, y, z, a, b, c, s;
-    std::cout << "x:";
-    std::cin >> x;
-    std::cout << "y:";
-    std::cin >> y;
-    std::cout << "z:";
-    std::cin >> z;
+    cout << "x:";
+    cin >> x;
+    cout << "y:";
+    cin >> y;
+    cout << "z:";
+    cin >> z;
     a = 2 * cos(x - 2 / 3.0);
     b = 1 / 2.0 + sin(y) * sin(y);
     c = 1 + pow(z, 2) / (3 - pow(z, 2) / 5);
     s = a / b * c;
-    std::cout << "Result s=" << s << std::endl;
+    cout << "Result s=" << s << endl;
 }
 
---
+++ Lab-2-task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x, y, f, a, s;
+    int k;
+    cout << "x: "; cin >> x;
+    cout << "y: "; cin >> y;
+    cout << "f: 1 - sh(x), 2 - x^2, 3 – exp(x)"; cin >> k;
+    switch (k)
+    {
+        case 1: f = sinh(x); break;
+        case 2: f = pow(x, 2.0); break;
+        case 3: f = exp(x); break;
+        default: cout << "Function not chosen"; return 1;
+    }
+    a = fabs(x * y);
+    if (a < 5) {
+        cout << "No result" << endl;
+        return 1;
+    }
+    else if (a > 10) {
+        s = fabs(f) + log(y);
+    }
+    else if (a <= 10 && a > 5) {
+        s = exp(f + y);
+    }
+    else {
+        s = sin(x) + tan(y);
+    }
+    cout << "result = " << s << endl;
+    
+}
